fa3dc68b63b52f868de7d7524bdf20aade2aeeec
ZY-OMAR <m5321omar@gmail.com>
2024-11-19 01:44:53 +0200

    first commit

---
+++ mac_adderss.py
@@ -0,0 +1,39 @@
+import optparse
+import subprocess
+import re
+
+# This is a reader that will take network interface and MAC address
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="network_interface", help="This place is for network interface")
+    parser.add_option("-m", "--mac", dest="new_mac", help="This place is for MAC address")
+    options, arguments = parser.parse_args()
+
+    if not options.network_interface:
+        parser.error("[-] Specify a network interface, type -h for help")
+
+    if not options.new_mac:
+        parser.error("[-] Specify MAC address, type -h for help")
+
+    return options
+# These are system commands that will change the MAC address for a network interface
+def mac_changer(network_interface, new_mac):
+    subprocess.call("ifconfig " + network_interface + " down", shell=True)
+    subprocess.call("ifconfig " + network_interface + " hw ether " + new_mac, shell=True)
+    subprocess.call("ifconfig " + network_interface + " up", shell=True)
+    print("[+] Changing MAC address for " + network_interface + " to " + new_mac)
+
+# Filtering MAC address
+def get_mac(network_interface):
+    ifconfig_result = subprocess.check_output("ifconfig " + network_interface, shell=True).decode("UTF-8")
+    mac_address = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    return mac_address[0]
+
+options = get_arguments()
+mac_changer(options.network_interface, options.new_mac)
+mac_address = get_mac(options.network_interface)
+
+if mac_address == options.new_mac:
+    print("[+] MAC address has changed successfully")
+else:
+    print("[-] Something went wrong...")
